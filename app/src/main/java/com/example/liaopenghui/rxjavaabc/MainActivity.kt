89d5b49ee275d00c8d200ab9b953280e1d495534
pony <467969@qq.com>
2018-12-28 15:41:30 +0800

    change to lambda express

--- app/src/main/java/com/example/liaopenghui/rxjavaabc/MainActivity.kt
+++ app/src/main/java/com/example/liaopenghui/rxjavaabc/MainActivity.kt
@@ -7,14 +7,11 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.BiFunction
-import io.reactivex.functions.Consumer
-import io.reactivex.functions.Function
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
-import java.util.concurrent.Callable
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
@@ -86,53 +83,30 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
             emitter.onNext(5)
             emitter.onNext(2)
             emitter.onNext(1)
-        }).subscribe(object : Consumer<Int> {
-            override fun accept(t: Int?) {
-                info { "onNext:$t" }
-            }
-        })
+        }).subscribe { t -> info { "onNext:$t" } }
     }
 
     //线程变换
     fun threadChange() {
-        Observable.create(object : ObservableOnSubscribe<String> {
-            override fun subscribe(emitter: ObservableEmitter<String>) {
-                emitter.onNext("lph~~~")
-                emitter.onNext("jay~~~")
-                emitter.onNext("mayday~~~")
-            }
-
+        Observable.create(ObservableOnSubscribe<String> { emitter ->
+            emitter.onNext("lph~~~")
+            emitter.onNext("jay~~~")
+            emitter.onNext("mayday~~~")
         }).subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Consumer<String> {
-                override fun accept(t: String?) {
-                    toast(t.toString())
-                }
-
-            })
+            .subscribe { t -> toast(t.toString()) }
     }
 
     //map操作符
     fun mapOprationSymble() {
         var total: Int = 0
-        Observable.create(object : ObservableOnSubscribe<String> {
-            override fun subscribe(emitter: ObservableEmitter<String>) {
-                emitter.onNext("5")
-                emitter.onNext("5")
-            }
-
-        }).map(object : Function<String, Int> {
-            override fun apply(t: String): Int {
-                return t.toInt()
-            }
-
-        }).subscribe(object : Consumer<Int> {
-            override fun accept(t: Int?) {
-                total += t!!
-                info { "total:$total" }
-            }
-
-        })
+        Observable.create(ObservableOnSubscribe<String> { emitter ->
+            emitter.onNext("5")
+            emitter.onNext("5")
+        }).map { t -> t.toInt() }.subscribe { t ->
+            total += t!!
+            info { "total:$total" }
+        }
     }
 
     //concat操作符
@@ -166,74 +140,40 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
 
     //concat操作符
     fun getObservableA(o: Any?): Observable<String> {
-        return Observable.fromCallable(object : Callable<String> {
-            override fun call(): String {
-                Thread.sleep(1000)//假设为耗时操作
-                return "liaopenghui"
-            }
-
-        })
+        return Observable.fromCallable {
+            Thread.sleep(1000)//假设为耗时操作
+            "liaopenghui"
+        }
     }
 
     //concat操作符
     fun getObservableB(o: Any?): Observable<String> {
-        return Observable.fromCallable(object : Callable<String> {
-            override fun call(): String {
-                Thread.sleep(1000)//假设为耗时操作
-                return "jay jay jay"
-            }
-
-        })
+        return Observable.fromCallable {
+            Thread.sleep(1000)//假设为耗时操作
+            "jay jay jay"
+        }
     }
 
     //flatMap 拿着上个函数的结果 作为下一个函数的参数
     fun flatMapOprationSymble() {
-        Observable.create(object : ObservableOnSubscribe<String> {
-            override fun subscribe(emitter: ObservableEmitter<String>) {
-                emitter.onNext("123456789111")
+        Observable.create(ObservableOnSubscribe<String> { emitter -> emitter.onNext("123456789111") }).flatMap { t ->
+            if (t == "123456789") {
+                ObservableSource<String> { observer -> observer.onNext("密码正确") }
+            } else {
+                ObservableSource<String> { observer -> observer.onNext("密码错误") }
             }
-        }).flatMap(object : Function<String,ObservableSource<String>>{
-            override fun apply(t: String): ObservableSource<String> {
-                if(t == "123456789"){
-                    return object :ObservableSource<String>{
-                        override fun subscribe(observer: Observer<in String>) {
-                            observer.onNext("密码正确")
-                        }
-                    }
-                }else{
-                    return object :ObservableSource<String>{
-                        override fun subscribe(observer: Observer<in String>) {
-                            observer.onNext("密码错误")
-                        }
-                    }
-                }
-            }
-
-        }).subscribeOn(AndroidSchedulers.mainThread()).subscribe(object :Consumer<String>{
-            override fun accept(t: String?) {
-                toast(t.toString())
-            }
-
-
-        })
+        }.subscribeOn(AndroidSchedulers.mainThread()).subscribe { t -> toast(t.toString()) }
     }
 
-    fun zipOprationSymble(){
-        Observable.zip(getObservableA(null),getObservableB(null),object :BiFunction<String,String,String>{
-            override fun apply(t1: String, t2: String): String {
-                return t1+t2
-
-            }
-        }).subscribeOn(AndroidSchedulers.mainThread()).subscribe(object :Consumer<String>{
-            override fun accept(t: String?) {
-                toast(t.toString())
-            }
-        })
+    fun zipOprationSymble() {
+        Observable.zip(getObservableA(null), getObservableB(null),
+            BiFunction<String, String, String> { t1, t2 -> t1 + t2 }).subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe { t -> toast(t.toString()) }
     }
 
 
-    fun intervalOprationSymble(){
-         disposable = Flowable.interval(1, TimeUnit.SECONDS).doOnNext { t -> error("accept: doOnNext : $t") }
+    fun intervalOprationSymble() {
+        disposable = Flowable.interval(1, TimeUnit.SECONDS).doOnNext { t -> error("accept: doOnNext : $t") }
             .subscribe { t -> error("accept: accept : $t") }
     }
 
